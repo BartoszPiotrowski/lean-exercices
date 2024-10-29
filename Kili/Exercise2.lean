add33a84900799c2a479ccb72d5d2e61ad6ab26a
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2024-10-29 10:47:34 +0100

    2 done

--- Kili/Exercise2.lean
+++ Kili/Exercise2.lean
@@ -1,8 +1,13 @@
+import Mathlib
+import Mathlib.Data.Nat.Choose.Basic
+open Nat
 
 /-
-Please solve this coding question in Lean 4 (Paste your solution or link to a GitHub repository):
-
 Let $n$ and $k$ be nonnegative integers with $k \leqslant n$. Then
 (i ) $\binom{n}{0}=\binom{n}{n}=1$
 (ii) $\binom{n}{k}=\binom{n}{n-k}$.
 -/
+
+example (n k : ℕ) (_ : k ≤ n) : choose n n = choose n 0 := by exact (choose_symm_of_eq_add rfl)
+example (n k : ℕ) (_ : k ≤ n) : choose n n = 1 := by exact choose_self n
+example (n k : ℕ) (h : k ≤ n) : choose n (n - k) = choose n k := by exact choose_symm h
