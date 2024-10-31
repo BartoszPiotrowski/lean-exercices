2e731f9c22ce737b93e31263b8c47c88d40ec70b
Bartosz Piotrowski <bartoszpiotrowski@post.pl>
2024-10-31 13:54:20 +0100

    clean up

--- Kili.lean
+++
@@ -1,6 +0,0 @@
--- This module serves as the root of the `Kili` library.
--- Import modules here that should be built as part of the library.
-import Kili.Exercise0
-import Kili.Exercise1
-import Kili.Exercise2
-import Kili.Exercise3
